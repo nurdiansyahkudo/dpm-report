0626a1ce747c51db869da7ad7a6ab687c3dfcf5a
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-12 13:10:35 +0700

    Fix Access Rights

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import account_move
 from . import stock_picking
+from . import product_template
---
+++ models/product_template.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+class ProductTemplate(models.Model):
+  _inherit = "product.template"
+
+  standard_price = fields.Float(
+        'Cost', compute='_compute_standard_price',
+        inverse='_set_standard_price', search='_search_standard_price',
+        digits='Product Price', groups="product.template stock_manager",
+        help="""Value of the product (automatically computed in AVCO).
+        Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
+        Used to compute margins on sale orders.""")
